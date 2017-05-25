1a15d7452d59845f572b01d779807e5171c8ef5b
jenine harris <statzgrl@gmail.com>
2017-05-25 14:14:51 -0500

    Update Paper1.R

--- Paper1.R
+++ Paper1.R
@@ -8,7 +8,7 @@
 ##CODE INFORMATION##
 
 #Written by: Sarah Wondmeneh and Yiqiang Zhao#
-#Github Link: #
+#Github Link: https://github.com/statzgrl/Reproducible-Research/blob/master/Paper1.R
 
 ############################################
 
